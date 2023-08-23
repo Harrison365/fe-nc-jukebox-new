9812773806bd39ecfc94f511fccdc1193759e8c8
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-08-23 10:12:08 +0100

    to task 10

--- src/App.js
+++ src/App.js
@@ -1,54 +1,59 @@
 const App = () => {
-  const playListTitle = 'My cool playlist';
+  const playListTitle = "My cool playlist";
   const songs = [
     {
       id: 1,
-      title: 'Dreams',
-      artist: 'Fleetwood Mac',
+      title: "Dreams",
+      artist: "Fleetwood Mac",
       albumCover:
-        'https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG',
+        "https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG",
       previewURL:
-        'https://open.spotify.com/embed/track/0ofHAoxe9vBkTCp2UQIavz?utm_source=generator',
+        "https://open.spotify.com/embed/track/0ofHAoxe9vBkTCp2UQIavz?utm_source=generator",
     },
     {
       id: 2,
       title: "Don't stop me now",
-      artist: 'Queen',
+      artist: "Queen",
       albumCover:
-        'https://upload.wikimedia.org/wikipedia/en/9/97/Queen-dont-stop-me-now-1979-6-s.jpg',
+        "https://upload.wikimedia.org/wikipedia/en/9/97/Queen-dont-stop-me-now-1979-6-s.jpg",
       previewURL:
-        'https://open.spotify.com/embed/track/5T8EDUDqKcs6OSOwEsfqG7?utm_source=generator',
+        "https://open.spotify.com/embed/track/5T8EDUDqKcs6OSOwEsfqG7?utm_source=generator",
     },
     {
       id: 3,
-      title: 'Enter Sandman',
-      artist: 'Metallica',
+      title: "Enter Sandman",
+      artist: "Metallica",
       albumCover:
-        'https://upload.wikimedia.org/wikipedia/en/2/2c/Metallica_-_Metallica_cover.jpg',
+        "https://upload.wikimedia.org/wikipedia/en/2/2c/Metallica_-_Metallica_cover.jpg",
       previewURL:
-        'https://open.spotify.com/embed/track/5sICkBXVmaCQk5aISGR3x1?utm_source=generator',
+        "https://open.spotify.com/embed/track/5sICkBXVmaCQk5aISGR3x1?utm_source=generator",
     },
     {
       id: 4,
-      title: 'Sledgehammer',
-      artist: 'Peter Gabriel',
+      title: "Sledgehammer",
+      artist: "Peter Gabriel",
       albumCover:
-        'https://upload.wikimedia.org/wikipedia/en/c/cf/Sledgehammer_Cover.jpg',
+        "https://upload.wikimedia.org/wikipedia/en/c/cf/Sledgehammer_Cover.jpg",
       previewURL:
-        'https://open.spotify.com/embed/track/029NqmIySn1kOY305AAhxT?utm_source=generator',
+        "https://open.spotify.com/embed/track/029NqmIySn1kOY305AAhxT?utm_source=generator",
     },
   ];
   const playCounts = {
     Dreams: 10,
-    'Enter Sandman': 5,
+    "Enter Sandman": 5,
     "Don't stop me now": 8,
     Sledgehammer: 11,
   };
+
   const currentlyPlaying = "Don't stop me now";
 
   return (
     <div className="App">
-      <h1>React Intro</h1>
+      <Header title={playListTitle} />
+      <div className="song-list">
+        <Songs songs={songs} currentlyPlaying={currentlyPlaying} />
+      </div>
+      <PlayCounts playCounts={playCounts} />
     </div>
   );
 };
@@ -56,3 +61,50 @@ const App = () => {
 export default App;
 
 // Create your new Components here
+
+const Header = (props) => {
+  return <h1>{props.title}</h1>;
+};
+const Songs = (props) => {
+  return (
+    <div className="song-list">
+      {props.songs.map((song) => {
+        return (
+          <div
+            className={
+              props.currentlyPlaying === song.title
+                ? "song-item song-current"
+                : "song-item"
+            }
+            key={song.id}
+          >
+            <h2>{song.title}</h2>
+            <p>{song.artist}</p>
+            <img className="song-img" src={song.albumCover} alt="album cover" />
+            {props.currentlyPlaying === song.title ? <p>(Playing)</p> : ""}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const PlayCounts = (props) => {
+  const totalPlays = Object.values(props.playCounts).reduce(
+    (acc, cur) => acc + cur,
+    0
+  );
+  return (
+    <div className="play-counts">
+      <h2>Play Counts</h2>
+      <ul>
+        {Object.keys(props.playCounts).map((songTitle, index) => (
+          <li key={index}>
+            {songTitle}: {props.playCounts[songTitle]} plays
+          </li>
+        ))}
+      </ul>
+      <div>Total plays: {totalPlays}</div>
+    </div>
+  );
+};
